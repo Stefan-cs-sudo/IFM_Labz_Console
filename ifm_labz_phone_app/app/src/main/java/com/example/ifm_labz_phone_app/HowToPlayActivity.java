40090cb57af1e44c6e29d3a1078c4ca139aac9f7
popam482 <popamaria482@gmail.com>
2026-05-09 23:38:02 +0300

    app main menu, how to play and about us display

---
+++ ifm_labz_phone_app/app/src/main/java/com/example/ifm_labz_phone_app/HowToPlayActivity.java
@@ -0,0 +1,41 @@
+package com.example.ifm_labz_phone_app;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HowToPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_how_to_play);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView tvRulesContent = findViewById(R.id.tvRulesContent);
+
+        tvRulesContent.setText(Html.fromHtml(tvRulesContent.getText().toString(), Html.FROM_HTML_MODE_LEGACY));
+
+        Button btnBack = findViewById(R.id.btnBack);
+        if (btnBack != null) {
+            btnBack.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
+    }
+}
